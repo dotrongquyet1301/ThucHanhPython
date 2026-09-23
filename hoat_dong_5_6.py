885e6a8e0522c3bcaf0cf5a519e0581365e9f7ed
dotrongquyet1301 <2411062461@hunre.edu.vn>
2026-09-23 10:26:59 +0700

    Add files via upload

---
+++ hoat_dong_5_6.py
@@ -0,0 +1,50 @@
+#hoạt động 5
+#bài tập 5.1
+diem = 6.5
+if diem >= 8.0:
+    pass # chua cai dat logic cho truong hop nay, se bo sung sau
+elif diem >= 5.0:
+    print("Dat yeu cau")
+else:
+    pass
+#bài tập 5.2
+diem = 6.5
+if diem >= 8.0:
+    pass # chua cai dat logic cho truong hop nay, se bo sung sau
+elif diem >= 5.0:
+    print("Dat yeu cau")
+else:
+    pass
+#bài tập 5.3
+diem = 6.5
+if diem >= 8.0:
+    pass # chua cai dat logic cho truong hop nay, se bo sung sau
+elif diem >= 5.0:
+    print("Dat yeu cau")
+else:
+    pass
+#bài tập 5.4
+diem = 6.5
+if diem >= 8.0:
+    pass # chua cai dat logic cho truong hop nay, se bo sung sau
+elif diem >= 5.0:
+    print("Dat yeu cau")
+else:
+    pass
+#hoạt động 6
+#bài tập 6.1
+diem = 6.5
+if diem >= 8.0:
+    pass # chua cai dat logic cho truong hop nay, se bo sung sau
+elif diem >= 5.0:
+    print("Dat yeu cau")
+else:
+    pass
+#bài tập 6.2
+diem = 6.5
+if diem >= 8.0:
+    pass # chua cai dat logic cho truong hop nay, se bo sung sau
+elif diem >= 5.0:
+    print("Dat yeu cau")
+else:
+    pass
